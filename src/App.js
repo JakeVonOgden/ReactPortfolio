45c7e334a58fca914e1032c5fd1d1d69b3911323
Jacob Von Ogden <jvon1197@gmail.com>
2021-12-10 09:58:43 -0500

    Set up folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Header, Landing, About, Projects, Contact, Footer} from './components';
 
 const App = () => {
   return (
     <div className="App">
-        Hello World
+      <Header />
+      <Landing />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Header } from './Header/Header';
+export {default as Landing } from './Landing/Landing';
+export {default as About } from './About/About';
+export {default as Projects } from './Projects/Projects';
+export {default as Contact } from './Contact/Contact';
+export {default as Footer } from './Footer/Footer';
