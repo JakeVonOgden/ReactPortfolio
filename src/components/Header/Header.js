76c1793181f65aaf2eeb6db482d50fbdde78b5a7
Jacob Von Ogden <jvon1197@gmail.com>
2021-12-11 04:08:57 -0500

    header and landing page rough draft finished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import './header.css'
+import './header.css';
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className='header'>
+            <div className='header__content'>
+                <div className='header__logo-container'>
+                    <div className='header__logo-content'>Jacob Von Ogden</div>
+                </div>
+                <div className='header__main'>
+                    <ul className='header__links'>
+                        <li class='header__link-wrapper'>
+                            <a href='/' className='header__link'>Home</a>
+                        </li>
+                        <li class='header__link-wrapper'>
+                            <a href='/#about' className='header__link'>About</a>
+                        </li>
+                        <li class='header__link-wrapper'>
+                            <a href='/#projects' className='header__link'>Projects</a>
+                        </li>
+                        <li class='header__link-wrapper'>
+                            <a href='/#contact' className='header__link'>Contact</a>
+                        </li>
+                    </ul>
+                    <div className='header__main-ham-menu-content d-none'>
+                        <img src='https://d33wubrfki0l68.cloudfront.net/79e75114856ae61628d2ad26504e3ff4ab2c71b6/f06a4/assets/svg/ham-menu.svg' alt='hamburger menu' className='header__main-ham-menu' />
+                        <img src='https://d33wubrfki0l68.cloudfront.net/de2a681c8ca1630b29949b3a34bf158a686a0554/6a0ec/assets/svg/ham-menu-close.svg' alt='hamburger menu close' className='header__main-ham-menu-close d-none' />
+                    </div>
+                </div>
+                <div className='header__sm-menu'>
+                    <div className='header__sm-menu-content'>
+                        <ul className='header__sm-menu-links'>
+                            <li className='header__sm-menu-links'>
+                                <a href='/' className='header__link'>Home</a>
+                            </li>
+                            <li className='header__sm-menu-links'>
+                                <a href='/#about' className='header__link'>About</a>
+                            </li>
+                            <li className='header__sm-menu-links'>
+                                <a href='/#projects' className='header__link'>Projects</a>
+                            </li>
+                            <li className='header__sm-menu-links'>
+                                <a href='/#contact' className='header__link'>Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,9 +3,36 @@ import './landing.css';
 
 const Landing = () => {
     return (
-        <div>
-            Landing
-        </div>
+        <section className='home-hero'>
+            <div className='home-hero__content'>
+                <h1 className='heading-primary'>Hey, I'm Jacob Von Ogden</h1>
+                <div className='home-hero__info'>
+                    <p className='text-primary'>
+                        A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product
+                    </p>
+                </div>
+                <div className='home-hero__cta'>
+                    <a href='/#projects' className='btn btn--bg'>Projects</a>
+                </div>
+            </div>
+            <div className='home-hero__socials'>
+                <div className='home-hero__social'>
+                    <a href='https://www.linkedin.com/in/jacob-von-ogden-b3b191219/' className='home-hero__social-icon-link' rel='noreferrer' target='_blank'>
+                        <img src='https://d33wubrfki0l68.cloudfront.net/7f29579dde49e02480372aa49f7189c5536b0118/34b92/assets/png/linkedin-ico.png' alt="Jacob Von Ogden Linkedin" className='home-hero__social-icon' />
+                    </a>
+                </div>
+                <div className='home-hero__social'>
+                    <a href='https://github.com/JakeVonOgden' className='home-hero__social-icon-link' rel='noreferrer' target='_blank'>
+                        <img src='https://d33wubrfki0l68.cloudfront.net/5557d5a11584d7201a38ee1a95200f57a4cc0f88/15085/assets/png/github-ico.png' alt="Jacob Von Ogden Github" className='home-hero__social-icon' />
+                    </a>
+                </div>
+            </div>
+            <svg class="arrows">
+				<path class="a1" d="M0 0 L30 32 L60 0"></path>
+				<path class="a2" d="M0 20 L30 52 L60 20"></path>
+				<path class="a3" d="M0 40 L30 72 L60 40"></path>
+			</svg>
+        </section>
     )
 }
 
